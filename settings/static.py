22e1ce70754413372d58487165778c5494fcadf7
Evandro Myller <eMyller@7ws.co>
2016-05-09 20:50:50 -0300

    Set up templating

--- settings/static.py
+++ settings/static.py
@@ -1,5 +1,27 @@
 from decouple import config
 
+from .base import BASE_DIR, DEBUG
+
 
 # URL to serve static files
 STATIC_URL = config('STATIC_URL', default='/static/')
+
+
+# Template finders and processors
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR.child('frontend', 'templates'),
+        ],
+        'OPTIONS': {
+            'debug': DEBUG,
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
